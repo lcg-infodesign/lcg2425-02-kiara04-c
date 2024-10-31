1287aa89875a72b336e00403245d4b8ca07e7e4d
kiara04-c <chiara.cucchetti04@gmail.com>
2024-10-31 14:48:40 +0100

    final version

--- sketch.js
+++ sketch.js
@@ -1,54 +1,37 @@
-let colors = [
-  '#FF8C00', // Arancio
-  '#4B0082', // Viola scuro
-  '#A52A2A', // Marrone
-  '#FF4500', // Rosso pomodoro
-  '#FFD700', // Giallo oro
-  '#8B4513'  // Marrone cioccolato
+let colors = [ 
+  '#ffa537', // Arancio
+  '#732587', // Viola scuro
+  '#8f3838', // Marrone
+  '#caae15', // Giallo oro
 ];
 
 function setup() { 
   createCanvas(windowWidth, windowHeight);
   noLoop();
-  // Cambia disegno dopo un intervallo di tempo in automatico
   setInterval(() => {
     location.reload();
   }, 2000);
 }
 
 function draw() {
-  background("#ffdeb3");
+  background("#fdf2df");
   noFill();
   blendMode(MULTIPLY);
   strokeWeight(3);
+  
+  //array delle posizioni verticali (y) di ogni serie di glifi
+  const verticalPositions = [70, 170, 270, 370, 470, 570];
 
-  // Rigenera le righe di glifi
   for (let i = 0; i < 20; i++) {
-    stroke(random(colors)); // Scegli un colore casuale dall'array
-    push();
-    translate((70 * i) + 70, 70);
-    repetition(random(1, 3), 50, random(3, 5));
-    pop();
-    push();
-    translate((70 * i) + 70, 170);
-    repetition(random(1, 3), 50, random(3, 5));
-    pop();
-    push();
-    translate((70 * i) + 70, 270);
-    repetition(random(1, 3), 50, random(3, 5));
-    pop();
-    push();
-    translate((70 * i) + 70, 370);
-    repetition(random(1, 3), 50, random(3, 5));
-    pop();
-    push();
-    translate((70 * i) + 70, 470);
-    repetition(random(1, 3), 50, random(3, 5));
-    pop();
-    push();
-    translate((70 * i) + 70, 570);
-    repetition(random(1, 3), 50, random(3, 5));
-    pop();
+    let xOffset = (70 * i) + 70;
+
+    //forEach applica una certa azione per ogni elemento dell'array
+    verticalPositions.forEach(yOffset => {
+      stroke(random(colors));
+      translate(xOffset, yOffset);
+      repetition(random(3, 6), 50, random(3, 5));
+      resetMatrix(); // Resetta la matrice per evitare l'accumulo delle trasformazioni
+    });
   }
 }
 
